c19e0db2d0639c705535046014492b726bcd1041
Olawole Dosunmu <olawoledosunmu@gmail.com>
2023-09-09 17:15:50 +0100

    First commit

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ def hng_stage_one():
     today = datetime.date.today().strftime('%A')
     utc_time = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
 
-    github_file_url = ""
-    github_repo_url = ""
+    github_file_url = "https://github.com/Lawole/HNG_stage_one/blob/master/main.py"
+    github_repo_url = "https://github.com/Lawole/HNG_stage_one"
 
     response = {
                 "slack_name": slack_name,
                 "current_day": today,
                 "utc_time": utc_time,
                 "track": track,
-                "github_file_url": "https://github.com/Lawole/HNG_stage_one/blob/master/main.py",
-                "github_repo_url": "https://github.com/Lawole/HNG_stage_one",
+                "github_file_url": github_file_url,
+                "github_repo_url": github_repo_url,
                 "status_code": 200
             }
 
