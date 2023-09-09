f85b80027b48fe598605b7e7a756917b72385d45
Olawole Dosunmu <olawoledosunmu@gmail.com>
2023-09-09 15:31:19 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+import datetime
+
+
+app = Flask(__name__)
+
+
+@app.route("/api", methods=["GET"])
+def hng_stage_one():
+    # parameters
+    slack_name = str(request.args.get("slack_name"))
+    track = str(request.args.get("track"))
+
+    today = datetime.date.today().strftime('%A')
+    utc_time = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    github_file_url = ""
+    github_repo_url = ""
+
+    response = {
+                "slack_name": slack_name,
+                "current_day": today,
+                "utc_time": utc_time,
+                "track": track,
+                "github_file_url": " ",
+                "github_repo_url": " ",
+                "status_code": 200
+            }
+
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
